104034524f014076ea5ad0265f367d31bf6659b5
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-08-07 16:46:24 +0200

    iOS setup

--- quakes_google_maps/ios/Runner/AppDelegate.m
+++ quakes_google_maps/ios/Runner/AppDelegate.m
@@ -1,13 +1,13 @@
-#import "AppDelegate.h"
-#import "GeneratedPluginRegistrant.h"
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyCaZM6IQHbBA1JNyk-_GZ0hVIZGSZBuOZY"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
-
 @end
