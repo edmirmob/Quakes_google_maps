d5e397c9122597b3ff757a1f73cd501f853fcd19
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-08-08 17:37:49 +0200

    Implementing CameraPosition animations

--- quakes_google_maps/lib/maps/google_maps/show_map.dart
+++ quakes_google_maps/lib/maps/google_maps/show_map.dart
@@ -12,6 +12,7 @@ class _ShowMapState extends State<ShowMap> {
   GoogleMapController mapController;
   static final LatLng _center = LatLng(43.85328125938318, 18.374909033640293);
   static final LatLng _anotherPlace = LatLng(43.853776, 18.366498);
+  static final LatLng _stadiumLocation = LatLng(43.846851, 18.387655);
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
@@ -23,25 +24,41 @@ class _ShowMapState extends State<ShowMap> {
         title: Text('Maps'),
       ),
       body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(target: _center, zoom: 13),
-          mapType: MapType.normal,
-          markers: {sarajevoMarker, sarajevoMarkerSecond},
-          ),
-          
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: _center, zoom: 13),
+        mapType: MapType.normal,
+        markers: {sarajevoMarker, sarajevoMarkerSecond},
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToStadium,
+        label: Text('Stadium Grbavica'),
+        icon: Icon(Icons.place),
+      ),
     );
   }
 
+  static final CameraPosition _stadiumPosition = CameraPosition(
+    target: _stadiumLocation,
+    zoom: 16.80,
+    tilt: 34,
+    bearing: 190
+  );
+  Future<void> _goToStadium() async {
+    final GoogleMapController controller = await mapController;
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      _stadiumPosition,
+    ));
+  }
+
   Marker sarajevoMarker = Marker(
-    markerId: MarkerId('Sarajevo'),
-    position: _center,
-    infoWindow: InfoWindow(title: 'Sarajevo', snippet: 'This is a great town!'),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta)
-  );
-   Marker sarajevoMarkerSecond = Marker(
-    markerId: MarkerId('Sarajevo area'),
-    position: _anotherPlace,
-    infoWindow: InfoWindow(title: 'Sarajevo area', snippet: 'This is area!'),
-    icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta)
-  );
+      markerId: MarkerId('Sarajevo'),
+      position: _center,
+      infoWindow:
+          InfoWindow(title: 'Sarajevo', snippet: 'This is a great town!'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta));
+  Marker sarajevoMarkerSecond = Marker(
+      markerId: MarkerId('Sarajevo area'),
+      position: _anotherPlace,
+      infoWindow: InfoWindow(title: 'Sarajevo area', snippet: 'This is area!'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta));
 }
